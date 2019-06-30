88b0fdb7ef13f2ce258a2023c4f46d62dd72f9f9
Joshua Beninson <joshuabeninson@gmail.com>
2019-06-30 12:57:23 -0700

    deduce role arn

--- env/dev/role.tf
+++ env/dev/role.tf
@@ -1,5 +1,5 @@
 # The SAML role to use for adding users to the ECR policy
-variable "saml_role" {}
+# variable "saml_role" {}
 
 # creates an application role that the container/task runs as
 resource "aws_iam_role" "app_role" {
@@ -29,6 +29,33 @@ data "aws_iam_policy_document" "app_policy" {
 
 data "aws_caller_identity" "current" {}
 
+locals {
+  caller_arn = "${data.aws_caller_identity.current.arn}"
+  # split the caller_arn into its elements
+  # caller_arn_split = [
+  #     arn:aws:sts::552242929734:assumed-role,
+  #     DevOps,
+  #     jbeninsonDEV
+  # ]
+  caller_arn_split = "${split("/","${local.caller_arn}")}"
+
+  # extract role name from caller_arn_split -> 'DevOps'
+  role_name = "${element("${local.caller_arn_split}", 1)}"
+  account_id = "${data.aws_caller_identity.current.account_id}"
+
+  role_arn = "arn:aws:iam::${local.account_id}:role/${local.role_name}"
+  common_tags = {
+      Service = "${var.app}"
+      Owner   = "${local.role_name}"
+      Terraform = "True"
+  }
+}
+
+
+# data "aws_iam_role" "role" {
+#   name = "${local.role_name}"
+# }
+
 # allow role to be assumed by ecs and local saml users (for development)
 data "aws_iam_policy_document" "app_role_assume_role_policy" {
   statement {
@@ -42,8 +69,11 @@ data "aws_iam_policy_document" "app_role_assume_role_policy" {
     principals {
       type = "AWS"
 
+      # identifiers = [
+      #   "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/${var.saml_role}/me@example.com",
+      # ]
       identifiers = [
-        "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/${var.saml_role}/me@example.com",
+        "${local.role_arn}",
       ]
     }
   }
