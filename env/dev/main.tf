b959c02672dc2673d3080500b8ee2f29e27a0641
Joshua Beninson <jbeninson@navexglobal.com>
2019-07-02 16:31:43 -0700

    adding vpc to output

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -33,22 +33,26 @@ output "status" {
   value = "fargate service info"
 }
 
-# Command to deploy a new task definition to the service using Docker Compose
-output "deploy" {
-  value = "fargate service deploy -f docker-compose.yml"
-}
+# # Command to deploy a new task definition to the service using Docker Compose
+# output "deploy" {
+#   value = "fargate service deploy -f docker-compose.yml"
+# }
 
-# Command to scale up cpu and memory
-output "scale_up" {
-  value = "fargate service update -h"
-}
+# # Command to scale up cpu and memory
+# output "scale_up" {
+#   value = "fargate service update -h"
+# }
 
-# Command to scale out the number of tasks (container replicas)
-output "scale_out" {
-  value = "fargate service scale -h"
-}
+# # Command to scale out the number of tasks (container replicas)
+# output "scale_out" {
+#   value = "fargate service scale -h"
+# }
 
 # Command to set the AWS_PROFILE
 output "aws_profile" {
   value = "${var.aws_profile}"
+}
+
+output "vpc" {
+  value = "${module.vpc.vpc_id}"
 }
