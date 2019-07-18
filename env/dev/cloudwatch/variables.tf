0e7c829856fd60bf8dc61c5e93749203b0ae4301
Joshua Beninson <jbeninson@navexglobal.com>
2019-07-17 19:22:28 -0700

    refactoring cloudwatch module

--- env/dev/cloudwatch/variables.tf
+++ env/dev/cloudwatch/variables.tf
@@ -2,8 +2,15 @@ variable "sns_endpoint" {
   default = "https://slackappdevops.navex-dev.com/sns"
 }
 
-
 variable "accountnumber" {
   description = "the account to publish/subscribe sns to"
   default = "552242929734"
+}
+
+variable "app" {
+  default = "SlackApp"
+}
+
+variable "environment" {
+    default = "dev"
 }
